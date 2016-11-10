23cadf6d8cb96cf0d36b64c41d32d1134301a731
chiehan <chieh.an.hsu@gmail.com>
2016-11-10 17:20:54 +0800

    first commit

---
+++ checkRepeatPb.js
@@ -0,0 +1,34 @@
+var pageRegex = /<pb id="([^<>]+)"/g;
+var pageIdStore = {};
+var pattern = process.argv[2];
+//console.log(pattern);
+//process.exit(1);
+
+var Path = require('path');
+var glob = require('glob');
+var fs = require('fs');
+
+var routes = glob.sync(pattern, {nosort: true});
+
+var files = routes.map((route) => {
+  let fileName = Path.basename(route);
+  let text = fs.readFileSync(route, 'utf8');
+
+  return {'fileName': fileName, 'text': text};
+});
+
+function alertReteatPage(file, i) {
+  file.text.replace(pageRegex, function(m, m1) {
+    var storedId = pageIdStore[m1];
+    var fileName = file.fileName;
+
+    if (!storedId) {
+      pageIdStore[m1] = fileName;
+    }
+    else {
+      console.log(m1, storedId, fileName);
+    }
+  });
+}
+
+files.map(alertReteatPage);
